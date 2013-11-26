d0a10d89fd32dacd46b79c7b0208f1d2a2d00bc0
jeremyBass <jeremybass@cableone.net>
2013-11-26 11:52:28 -0800

    a small refactor in code style

--- app/code/community/Wsu/MailingServices/controllers/LogController.php
+++ app/code/community/Wsu/MailingServices/controllers/LogController.php
@@ -2,7 +2,10 @@
 class Wsu_MailingServices_LogController extends Mage_Adminhtml_Controller_Action {
     protected function _initAction() {
         // load layout, set active menu and breadcrumbs
-        $this->loadLayout()->_setActiveMenu('system/tools')->_addBreadcrumb(Mage::helper('adminhtml')->__('System'), Mage::helper('adminhtml')->__('System'))->_addBreadcrumb(Mage::helper('adminhtml')->__('Tools'), Mage::helper('adminhtml')->__('Tools'))->_addBreadcrumb(Mage::helper('adminhtml')->__('Email Log'), Mage::helper('adminhtml')->__('Email Log'));
+        $this->loadLayout()->_setActiveMenu('system/tools')
+			->_addBreadcrumb(Mage::helper('adminhtml')->__('System'), Mage::helper('adminhtml')->__('System'))
+			->_addBreadcrumb(Mage::helper('adminhtml')->__('Tools'), Mage::helper('adminhtml')->__('Tools'))
+			->_addBreadcrumb(Mage::helper('adminhtml')->__('Email Log'), Mage::helper('adminhtml')->__('Email Log'));
         return $this;
     }
     public function indexAction() {
