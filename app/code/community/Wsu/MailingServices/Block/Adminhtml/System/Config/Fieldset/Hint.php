447dfee07ff7900cc57c4b3fb7e0a234094c77e9
jeremyBass <jeremybass@cableone.net>
2014-04-29 08:42:13 -0700

    had typo'ed the class here

--- app/code/community/Wsu/MailingServices/Block/Adminhtml/System/Config/Fieldset/Hint.php
+++ app/code/community/Wsu/MailingServices/Block/Adminhtml/System/Config/Fieldset/Hint.php
@@ -1,5 +1,5 @@
 <?php
-class Wsu_MailingServices_Adminhtml_System_Config_Fieldset_Hint extends Mage_Adminhtml_Block_Abstract
+class Wsu_MailingServices_Block_Adminhtml_System_Config_Fieldset_Hint extends Mage_Adminhtml_Block_Abstract
     implements Varien_Data_Form_Element_Renderer_Interface {
 	protected $_template = 'wsu/mailingservices/system/config/fieldset/hint.phtml';
     public function render(Varien_Data_Form_Element_Abstract $element){
