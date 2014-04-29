dead70b5b9d26a81d300539d438e61697db261af
jeremyBass <jeremybass@cableone.net>
2014-04-29 09:01:26 -0700

    corrects the gird scope

--- app/code/community/Wsu/MailingServices/Block/Log.php
+++ app/code/community/Wsu/MailingServices/Block/Log.php
@@ -1,7 +1,7 @@
 <?php
 class Wsu_MailingServices_Block_Log extends Mage_Adminhtml_Block_Widget_Grid_Container {
     public function __construct() {
-        $this->_blockGroup = 'mailingservices';
+        $this->_blockGroup = 'wsu_mailingservices';
         $this->_controller = 'log';
         $this->_headerText = Mage::helper('cms')->__('Email Log');
         parent::__construct();
